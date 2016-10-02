9c42db8b1995f746138b480479359a603ca9051a
Caio Siqueira da Silva <csiqueirasilva@gmail.com>
2016-10-02 10:01:19 -0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,46 @@
+var fs = require('fs')
+
+var regex = /^sat (\d): (\w{3}) (start|end)[ ]*: (.*)$/gm;
+
+function parseFile (file) {
+	
+	var data = fs.readFileSync("data/" + file, 'utf8');
+
+	data = data.replace(/[^]*Final results:/gm, '');
+	
+	var ret = [];
+
+	var match = regex.exec(data);
+
+	while(match !== null) {
+		var o = {};
+		o.sat = match[1];
+		o.type = match[2];
+		o.event = match[3];
+		o.date = new Date(match[4] + " GMT").getTime();
+		ret.push(o);
+		match = regex.exec(data);
+	}
+	
+	var outputFile = file.replace("TXT", "json");
+	
+	fs.writeFileSync("export/" + outputFile, JSON.stringify(ret));
+
+	console.log("Created: " + outputFile);
+	
+}
+
+fs.readdir("data/.", function(err, data) {
+	
+	if(err) {
+		return console.log(err);
+	}
+	
+	for(var i = 0; i < data.length; i++) {
+		if(data[i].match(/^.*\.TXT$/gm) !== null) {
+			console.log('reading file ' + data[i]);
+			parseFile(data[i]);
+		}
+	}
+	
+});
